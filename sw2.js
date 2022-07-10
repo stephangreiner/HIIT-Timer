57ec7d8d042a2514d6c40fa376d4989a97f23d98
stephangreiner <49695489+stephangreiner@users.noreply.github.com>
2022-07-10 18:38:27 +0200

    Add files via upload

---
+++ sw2.js
@@ -0,0 +1,51 @@
+// Perform install steps
+let CACHE_NAME = 'my-cache';
+let urlsToCache = [
+    '/style.css',    
+    'index.html',
+    '/HIIT-Timer/sw2.js',
+    "/HIIT-Timer/index.html",
+    "/HIIT-Timer/script.js",
+
+
+    ];
+
+self.addEventListener('install', function(event) {
+// Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(function(cache) {
+            console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          // Cache hit - return response
+          if (response) {
+            return response;
+          }
+          return fetch(event.request);
+        }
+      )
+    );
+  });
+
+  self.addEventListener('activate', function(event) {
+    var cacheWhitelist = ['pigment'];
+    event.waitUntil(
+      caches.keys().then(function(cacheNames) {
+        return Promise.all(
+          cacheNames.map(function(cacheName) {
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    );
+  });
