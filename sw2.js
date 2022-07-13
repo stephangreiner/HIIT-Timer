10fe06407982db84e81ff043fc77afd7ad464316
stephangreiner <49695489+stephangreiner@users.noreply.github.com>
2022-07-13 21:19:00 +0200

    Update sw2.js

--- sw2.js
+++ sw2.js
@@ -1,3 +1,17 @@
+import {registerRoute} from 'workbox-routing';
+import {CacheFirst} from 'workbox-strategies';
+import {RangeRequestsPlugin} from 'workbox-range-requests';
+
+registerRoute(
+  ({url}) => url.pathname.endsWith('.mp4'),
+  new CacheFirst({
+    plugins: [
+      new RangeRequestsPlugin(),
+    ],
+  });
+);
+
+
 // Set a name for the current cache
 var cacheName = 'v1'; 
 
